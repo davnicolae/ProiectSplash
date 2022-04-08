2e7615eb5073cbfd2d3745c593765802a0dd1d64
davnicolae <dav.nicolae96@gmail.com>
2022-04-08 11:24:00 +0300

    modificat pixel in rem

--- script.js
+++ script.js
@@ -38,13 +38,13 @@ $('.modal-toggle-2').on('click', function (e) {
 
 // accordion
 
-$('.accordion__header').click(function (e) {
+$('.accordion-header').click(function (e) {
   e.preventDefault();
   var currentIsActive = $(this).hasClass('is-active');
   $(this).parent('.accordion').find('> *').removeClass('is-active');
   if (currentIsActive != 1) {
     $(this).addClass('is-active');
-    $(this).next('.accordion__body').addClass('is-active');
+    $(this).next('.accordion-body').addClass('is-active');
   }
 });
 
